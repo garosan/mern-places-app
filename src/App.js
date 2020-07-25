aa171a39a6cc8665e67b7d9d611e4bf6dad9a809
Garo San <garosanchez27@gmail.com>
2020-07-25 01:02:38 -0500

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
-function App() {
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
