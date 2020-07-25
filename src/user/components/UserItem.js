01a4536b5b8f91687fe8cecdb8dbcbe3302a4467
Garo San <garosanchez27@gmail.com>
2020-07-25 01:16:46 -0500

    Add index.css, create USERS dummy data and pass it to UsersList component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = () => {
+  return <div>UserItem Component</div>;
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Garosan",
+      image: "https://pbs.twimg.com/profile_images/1273104407593390082/wZFRGIr4_400x400.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
