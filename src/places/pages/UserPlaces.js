66676947c659a302351ef5b64686b5e49547f7d5
Garo San <garosanchez27@gmail.com>
2020-07-25 19:18:51 -0500

    Create UserPlaces page, add styling and route

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Empire_State_Building_by_David_Shankbone.jpg/800px-Empire_State_Building_by_David_Shankbone.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Empire_State_Building_by_David_Shankbone.jpg/800px-Empire_State_Building_by_David_Shankbone.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
